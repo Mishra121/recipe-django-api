2077d2675e6ee5a3db5f7b40b4e5a9347ce818b4
Z0086L0 <Vibhu.Mishra@target.com>
2022-06-11 00:52:13 +0530

    UserSerializer and craete user endpoint added

--- app/core/admin.py
+++ app/core/admin.py
@@ -8,6 +8,7 @@ from django.utils.translation import gettext_lazy as _
 
 from core import models
 
+
 class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
@@ -40,5 +41,6 @@ class UserAdmin(BaseUserAdmin):
             ),
         }),
     )
+
 
 admin.site.register(models.User, UserAdmin)
