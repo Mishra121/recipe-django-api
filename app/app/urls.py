58699d17b5534ec2f735108c653995c51f946cce
Z0086L0 <Vibhu.Mishra@target.com>
2022-06-11 14:18:32 +0530

    recipe listing api and tests

--- app/app/urls.py
+++ app/app/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
          SpectacularSwaggerView.as_view(url_name='api-schema'),
          name='api-docs'),
     path('api/user/', include('user.urls')),
+    path('api/recipe/', include('recipe.urls')),
 ]
