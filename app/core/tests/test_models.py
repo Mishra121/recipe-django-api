e3ad725e634105d4dce1e1689c41a4c053715628
Z0086L0 <Vibhu.Mishra@target.com>
2022-06-11 18:51:01 +0530

    tags model and migrations added

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -8,6 +8,11 @@ from django.contrib.auth import get_user_model
 from core import models
 
 
+def create_user(email='user@example.com', password='testpass123'):
+    """return a new user."""
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         email = 'test@example.com'
@@ -59,3 +64,10 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test creating a tag is successful."""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
